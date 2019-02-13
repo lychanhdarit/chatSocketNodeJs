de1ea9b0903ff28cdf312bf8cd65266286ffabef
lychanhdarit <lychanhdarit@gmail.com>
2019-02-13 16:11:37 +0700

    update all 2a2

---
+++ SERVER.js
@@ -0,0 +1,132 @@
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.set("views","./views");
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(process.evn.PORT||8000,()=>{console.log("App running...");});
+
+var dataUser=[];
+
+io.on("connection",(socket)=>{
+	console.log("- New conection website.!!!");
+    
+    //lắng nghe tin nhắn
+	socket.on("client-send-data",(data)=>{
+        console.log(socket.id+": "+ data);
+        //io.sockets.emit trả cho tất cả
+		io.sockets.emit("server-send-data",{us:socket.username,msg:data});
+	});
+    //lắng nghe tin nhắn theo phong
+	socket.on("client-chat",(data)=>{
+        console.log(socket.id+": "+ data);
+        //io.sockets.in gửi theo room/socket
+        io.sockets.in(socket.roomName).emit("server-chat",{us:socket.username,msg:"<i style='color:#007793'>"+data+"</i>"});
+        
+      
+	});
+    //Lắng nghe đăng ký
+    socket.on("client-register",(data)=>{
+        if(dataUser.indexOf(data)>=0){
+            //Thất bại
+            //sockets.emit chỉ trả cho socket hiện tại
+            socket.emit("server-send-register-fail");
+            console.log("Username: "+data+" register exits.!!!");
+        }
+        else{
+            //Thành công thêm vào mảng
+            dataUser.push(data);
+            socket.username = data;//tự định nghĩa thêm username
+            socket.emit("server-send-register-success",data);
+            console.log("Username: "+data+" register success.!!!");
+        }
+        io.sockets.emit("server-send-list-user",dataUser);
+        
+        var dataRoom=[];
+        for (r in socket.adapter.rooms) {
+            dataRoom.push(r);
+        }
+
+        io.sockets.emit("server-send-list-room",dataRoom);
+    });
+    
+    //lắng nghe focus all
+	socket.on("client-focusIn-text",(data)=>{
+        //Xóa trong mảng
+        dataUser.splice(dataUser.indexOf(socket.username,1)); 
+        //Phát cho hàng xóm trừ mình
+        socket.broadcast.emit("server-send-focusIn",socket.username+" đang gõ...");
+     });
+     //lắng nghe focus room
+	socket.on("client-focusIn-room",(data)=>{
+        //Xóa trong mảng
+        dataUser.splice(dataUser.indexOf(socket.username,1)); 
+        //Phát cho hàng xóm trừ mình
+        io.sockets.in(socket.roomName).emit("server-send-focusIn-room",socket.username+" đang gõ...");
+     });
+    //lắng nghe focus
+    socket.on("client-focusOut-text",(data)=>{
+        //Xóa trong mảng
+        dataUser.splice(dataUser.indexOf(socket.username,1)); 
+        //Phát cho hàng xóm trừ mình
+        socket.broadcast.emit("server-send-focusOut",socket.username+" đang gõ");
+    });
+
+    //lắng nghe logout
+	socket.on("client-logout",(data)=>{
+       //Xóa trong mảng
+       dataUser.splice(dataUser.indexOf(socket.username,1)); 
+       //Phát cho hàng xóm trừ mình
+       socket.broadcast.emit("server-send-list-user",dataUser);
+    });
+
+    //lắng nghe tạo room
+    socket.on("client-leave-room",(data)=>{
+
+        socket.leave(data); 
+        console.log("leave room: "+data+"!");
+        var dataRoom=[];
+        for (r in socket.adapter.rooms) {
+            dataRoom.push(r);
+        }
+
+        io.sockets.emit("server-send-list-room",dataRoom);
+        socket.emit("server-send-room-socket",data);
+        //console.log(socket.adapter.rooms);//Log list rooom
+     });
+
+	socket.on("client-create-room",(data)=>{
+        socket.join(data);
+        socket.roomName = data;
+        console.log("Created room: "+data+"!");
+        var dataRoom=[];
+        for (r in socket.adapter.rooms) {
+            dataRoom.push(r);
+        }
+
+        io.sockets.emit("server-send-list-room",dataRoom);
+        socket.emit("server-send-room-socket",data);
+        //console.log(socket.adapter.rooms);//Log list rooom
+     });
+    
+    //Ngắt kết nối
+	socket.on("disconnect",()=>{
+        if(socket.username)
+        {
+            console.log(socket.username+": Disconnect!");
+            io.sockets.emit("server-send-list-user",dataUser);
+        }
+        else{
+            console.log(socket.id+": Disconnect!"); 
+            //Phát cho hàng xóm trừ mình
+            io.sockets.emit("server-send-list-user",dataUser);
+        }
+       
+        
+	});
+});
+
+app.get("/",function(req,res){
+	res.render("index");
+});
---
+++ public/xuly.js
@@ -0,0 +1,126 @@
+var socket = io("http://192.168.0.150:8000");
+        //lắng nghe chat tất cả
+		socket.on('server-send-data', function (data) {
+			$("#list-msg").append("<li><b>"+data.us+"</b>: "+data.msg+"</li>");
+        });
+        //lắng nghe chat phòng
+        socket.on('server-chat', function (data) {
+			$("#list-msg").append("<li><b>"+data.us+"</b>: "+data.msg+"</li>");
+        });
+
+        socket.on('server-send-focusIn', function (data) { 
+			$("#msg-box").html("<p><i>"+data+"</i></p>");
+        });
+
+        socket.on('server-send-focusIn-room', function (data) { 
+			$("#msg-box").html("<p><i>"+data+"</i></p>");
+        });
+
+        socket.on('server-send-focusOut', function (data) {
+			$("#msg-box").html("");
+        });
+        // lắng nghe đăng ký thất bại
+        socket.on('server-send-register-fail', function () {
+			alert("Đã có người đăng ký!");
+        });
+         // lắng nghe đăng ký thành công
+         socket.on('server-send-register-success', function (data) {
+			$("#loginForm").hide(2000);
+            $("#chatForm").show(1000);
+            $("#currentUser").html(data);
+
+        });
+        // lắng nghe danh sách online
+        socket.on('server-send-list-user', function (data) {
+            $("#list-user").empty();
+            data.forEach(i => {
+                $("#list-user").append("<li>"+i+" <small>online</small></li>");
+            });
+        });
+
+        // lắng nghe danh sách room 
+        socket.on('server-send-list-room', function (data) {
+            $("#list-room").empty(); 
+            data.forEach(i => { 
+                    $("#list-room").append("<li class='room' onclick=\"joinRoom(\'"+i+"\')\"> "+i+"</li>");
+            });
+        });
+
+        socket.on('server-send-room-socket', function (data) {
+            $("#currentRom").html("Message phòng: "+data);
+            $("#hiddenPhong").val(data);
+        });
+
+        function joinRoom(room){
+            var phong = $("#hiddenPhong").val();//
+            socket.emit('client-leave-room',phong);
+            socket.emit('client-create-room',room);
+        }
+		$(document).ready(function(){
+            $("#loginForm").show();
+            $("#chatForm").hide();
+
+            $("#btnReg").click(()=>{
+				socket.emit('client-register',$("#txtname").val());
+            });
+
+            $("#btnLogOut").click(()=>{
+                socket.emit('client-logout');
+                $("#loginForm").show(1000);
+                $("#chatForm").hide(2000);
+            });
+
+			$("#txtMessage").keyup(function(event) {
+				if(event.keyCode === 13) {
+                    $("#btn").click();
+                    socket.emit('client-focusOut-text');
+                }
+                var textMsg = $("#txtMessage").val();
+                if(textMsg.length >0)
+                {
+                    var phong = $("#hiddenPhong").val();//client-leave-room
+                    if(phong.length>0)
+                    {
+                        socket.emit('client-focusIn-room'); 
+                    }
+                    else{
+                        socket.emit('client-focusIn-text');
+                    }
+                   
+                }
+            }); 
+            
+            $("#txtMessage").focusout(function() {
+                socket.emit('client-focusOut-text');
+            }); 
+
+            $("#txtname").keyup(function(event) {
+				if(event.keyCode === 13) {
+					$("#btnReg").click();
+				}
+            });    
+            $("#txtRoom").keyup(function(event) {
+				if(event.keyCode === 13) {
+					$("#btnTaoRoom").click();
+				}
+            });    
+            //Gửi tin nhắn
+			$("#btn").click(()=>{
+                var phong = $("#hiddenPhong").val();//client-leave-room
+                if(phong.length>0)
+                {
+                     //Chat room
+                    socket.emit('client-chat',$("#txtMessage").val());   
+                }
+                else{
+                    //All chat all
+                    socket.emit('client-send-data',$("#txtMessage").val());
+                }
+				$("#txtMessage").val("");
+            });
+
+            $("#btnTaoRoom").click(()=>{
+				socket.emit('client-create-room',$("#txtRoom").val());
+            });
+            
+		});
